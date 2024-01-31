46f865cfea1f74302468618872a3c1d0127ddc81
greghim53lf <greghim53lf+github@gmail.com>
2024-01-31 01:31:28 +0100

    connected to databasw

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from "express";
-
 const app = express();
+
+import dotenv from "dotenv";
+dotenv.config()
+
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log(`MongoDb connected`);
+}).catch(err => console.log(err))
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
