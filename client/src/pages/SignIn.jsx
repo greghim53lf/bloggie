56dcffc1b75da1705b009d53d7ce281fcec92505
greghim53lf <greghim53lf+github@gmail.com>
2024-01-31 09:59:34 +0100

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,12 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
@@ -16,10 +16,8 @@ export default function SignIn() {
   }
   
   const [formData, setFormData] = useState(initialFormState)
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  // const { loading, error: errorMessage } = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     const { id, value } = e.target
@@ -27,55 +25,30 @@ export default function SignIn() {
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // if (!formData.email || !formData.password) {
-    //   return dispatch(signInFailure('Please fill all the fields'));
-    // }
-    // try {
-    //   dispatch(signInStart());
-    //   const res = await fetch('/api/auth/signin', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(formData),
-    //   });
-    //   const data = await res.json();
-    //   if (data.success === false) {
-    //     dispatch(signInFailure(data.message));
-    //   }
-
-    //   if (res.ok) {
-    //     dispatch(signInSuccess(data));
-    //     navigate('/');
-    //   }
-    // } catch (error) {
-    //   dispatch(signInFailure(error.message));
-    // }
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Missing field(s)")
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setIsLoading(true)
-      setErrorMessage(null)
-      const res = await fetch('api/auth/sign-in', {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/sign-in', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      })
-      const data = await res.json()
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message));
       }
-      setIsLoading(false)
-      setFormData(initialFormState)
-      if (data) {
-        navigate('/sign-in')
+
+      if (res.ok) {
+        dispatch(signInSuccess(data.data));
+        navigate('/');
       }
-    } catch ({message}) {
-      setIsLoading(false)
-      setErrorMessage(message);
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
   };
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -118,9 +91,9 @@ export default function SignIn() {
             <Button
               gradientDuoTone='pinkToOrange'
               type='submit'
-              disabled={isLoading}
+              disabled={loading}
             >
-              {isLoading ? (
+              {loading ? (
                 <>
                   <Spinner size='sm' />
                   <span className='pl-3'>Loading...</span>
